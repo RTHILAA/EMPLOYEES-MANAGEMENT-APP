b73a607ba75d5359acf1f7942731067d9e4cb5da
ANASS EL HARAZI <anaswins35@gmail.com>
2025-11-24 21:11:53 +0100

    update project

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,37 +1,39 @@
 import React from 'react'
 import "./List.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 export default function List() {
   return (
     <div className="list">
         <span className="title">Employees List :</span>
-            <table className="employees-table">
-                <tr>
-                    <th>Full Name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Department</th>
-                    <th>Position</th>
-                    <th>Hire Date</th>
-                    <th>Salary</th>
-                    <th>Status</th>
-                    <th>Actions</th>
-                </tr>
-                <tr>
-                    <td>John Doe</td>
-                    <td>gH6yS@example.com</td>
-                    <td>+1-202-555-0143</td>
-                    <td>Information Technology (IT)</td>
-                    <td>Software Developer</td>
-                    <td>2022-01-01</td>
-                    <td>$50000</td>
-                    <td>Active</td>
-                    <td>
-                        <button className="edit">Edit</button>
-                        <button className="delete">Delete</button>
-                    </td>
-                </tr>
-            </table>
+        <table className="employees-table">
+            <tr>
+                <th>Full Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Department</th>
+                <th>Position</th>
+                <th>Hire Date</th>
+                <th>Salary</th>
+                <th>Status</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <td>John Doe</td>
+                <td>gH6yS@example.com</td>
+                <td>+1-202-555-0143</td>
+                <td>IT</td>
+                <td>Software Developer</td>
+                <td>2022-01-01</td>
+                <td>$50000</td>
+                <td>Active</td>
+                <td className="actions">
+                    <FontAwesomeIcon icon={faPenToSquare} className="edit-icon" />
+                    <FontAwesomeIcon icon={faTrash} className="delete-icon" />
+                </td>
+            </tr>
+        </table>
     </div>
   )
 }
