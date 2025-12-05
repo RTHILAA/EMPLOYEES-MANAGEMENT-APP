04c2027f69b4560f3eb17f782ab8366e8358ae5f
ANASS EL HARAZI <anaswins35@gmail.com>
2025-12-05 03:26:38 +0100

    update project

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -18,24 +18,26 @@ export default function List({ employees }) {
                     <th>Salary</th>
                     <th>Status</th>
                     <th>Actions</th>
-               </tr>
-               {employees.map((emp) => (
-            <tr>
-              <td>{emp.fullname}</td>
-              <td>{emp.email}</td>
-              <td>{emp.phone}</td>
-              <td>{emp.department}</td>
-              <td>{emp.position}</td>
-              <td>{emp.hiredate}</td>
-              <td>{emp.salary}</td>
-              <td>{emp.status}</td>
-              <td className="actions">
-                <FontAwesomeIcon icon={faPenToSquare} className="edit-icon" />
-                <FontAwesomeIcon icon={faTrash} className="delete-icon" />
-              </td>
-            </tr>
-          ))}
-        </table>
+                </tr>
+                {employees.map((emp) => {
+                    return (
+                        <tr>
+                            <td>{emp.fullname}</td>
+                            <td>{emp.email}</td>
+                            <td>{emp.phone}</td>
+                            <td>{emp.department}</td>
+                            <td>{emp.position}</td>
+                            <td>{emp.hiredate}</td>
+                            <td>{emp.salary}</td>
+                            <td>{emp.status}</td>
+                            <td className="actions">
+                                <FontAwesomeIcon icon={faPenToSquare} className="edit-icon" />
+                                <FontAwesomeIcon icon={faTrash} className="delete-icon" />
+                            </td>
+                        </tr>
+                    )
+                })}
+            </table>
         </div>
-        );
+    );
 }
